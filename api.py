0d312cddda47fa6f76e7ec37e384bf8192f412eb
SoyDaviti <dschaguaro@hotmail.com>
2023-09-12 03:59:44 -0500

    primer commit

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from yahoo_data_fetcher import get_price
+from flask import request
 
 app = Flask(__name__)
 
@@ -7,9 +8,18 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/api/")
-def api():
-    return get_price("TSLA")
+@app.route("/api/<ticker>")
+def api(ticker):
+    return get_price(ticker)
+
+@app.route("/api/multiple/")
+def api_multiple():
+    tickers = request.args.get('tickers')
+    tickers = tickers.split(',')
+    result = []
+    for t in tickers:
+        result.append(get_price(t))
+    return result
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3500, debug=True)
